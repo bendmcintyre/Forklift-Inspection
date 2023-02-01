7c8257c6b5949209b3a68c538ae3433039baa72c
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-01-31 20:19:25 -0800

    merging form

---
+++ forklift-inspection/src/components/passfail.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function PassFail() {
+  const [ myTest, setMyTest] = useState("Pass");
+
+  const handleChange = (event) => {
+    setMyTest(event.target.value)
+    console.log(event.target.value)
+  }
+
+  return (
+    <form>
+      <select value={myTest} onChange={handleChange}>
+        <option value="Pass">Pass</option>
+        <option value="Fail">Fail</option>
+        <option value="NA">N/A</option>
+      </select>
+    </form>
+  )
+}
+
+export default PassFail;
+
+              
