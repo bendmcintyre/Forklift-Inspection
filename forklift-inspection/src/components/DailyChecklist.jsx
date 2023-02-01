158e830ff9868a26a93d06970845382aed61aa8e
anmichealgelo <anmichealgelo@gmail.com>
2023-02-01 11:39:53 +0800

    update: dailychecklist

--- forklift-inspection/src/components/DailyChecklist.jsx
+++ forklift-inspection/src/components/DailyChecklist.jsx
@@ -7,28 +7,84 @@ function DailyChecklist() {
     const [message, setMessage] = useState("");
 
     const API_URL = "https://forklift-inspection-backend.vercel.app";
-
+    
+    // Fetch all checklist records
     const getData = async () => {
         document.title = "Daily Checklist"
+
         const rawResponse = await fetch(API_URL + "/daily_checklist")
         const response = await rawResponse.json()
 
         if(response.count !== 0){    
             setData(response.data)
-        }else{
-            setMessage("No record found")
         }
-    } 
+    }
 
+    const refreshData = () => {
+        getData()
+    }
+    
     useEffect(() => {
         getData()
     }, [])
+
+    // Remove a checklist record by id
+    const removeData = async (id) => {
+        const rawResponse = await fetch(API_URL + "/daily_checklist/" + id, {
+            method: "DELETE"
+        })
+
+        const response = await rawResponse.json()
+        alert(response.message)
+    }
+
+    // Display data in table rows
+    const displayData = data.map(el => {
+        if(data.count <= 0){
+            return (
+                <tr key={data.count}>
+                    <td colSpan={9} className="text-center">No records found.</td>
+                </tr>
+            )
+        }else{
+            return (
+                <tr key={el._id}>
+                    <td><Link to={`/daily-checklist/${el._id}`}
+                        className="fw-semibold text-decoration-none">{el.truck_no}</Link></td>
+                    <td>{el.building_no}</td>
+                    <td>{el.shift}</td>
+                    <td>{el.is_internal_combustion ? '✔️' : ''}</td>
+                    <td>{el.is_electric ? '✔️' : ''}</td>
+                    <td>{el.hour_meter.total_hours}</td>
+                    <td>{el.operated_by}</td>
+                    <td>{el.supervised_by}</td>
+                    <td>
+                        <div className="d-flex">
+                            <Link to={`/daily-checklist/${el._id}`} 
+                                className="btn btn-primary btn-sm me-2">Edit</Link>
+                            <Button variant="danger" 
+                                size="sm" 
+                                className="fw-semibold" 
+                                onClick={() => removeData(el._id)}>Remove</Button>
+                        </div>
+                    </td>
+                </tr>
+            )
+        }
+    })
+
+    // const noRecord = <tr key={data.count}>
+    //     <td colSpan={9} className="text-center">No records found.</td>
+    // </tr>
 
     return (
         <Container>
             <div className="mb-4 d-flex justify-content-between align-items-center"> {/* got this from bootstrap docs */}
                 <h1>Daily Checklist</h1>
-                <Link to="/daily-checklist/new" className="btn btn-primary">Add new inspection</Link> {/* get and attach the classnames of bootstrap button*/}
+                <div>
+                    <Button variant="outline-primary" className="me-2" onClick={refreshData}>Refresh</Button>
+                    <Link to="/daily-checklist/new" className="btn btn-primary">Add new inspection</Link> {/* get and attach the classnames of bootstrap button*/}
+                </div>
             </div>
             <Table striped bordered hover>
                 <thead>
@@ -45,28 +101,8 @@ function DailyChecklist() {
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((el, index) => {
-                        return (
-                            <tr key={index}>
-                                <td><Link to={`/daily-checklist/${el._id}`}
-                                    className="fw-semibold text-decoration-none">{el.truck_no}</Link></td>
-                                <td>{el.building_no}</td>
-                                <td>{el.shift}</td>
-                                <td>{el.is_internal_combustion ? '✔️' : ''}</td>
-                                <td>{el.is_electric ? '✔️' : ''}</td>
-                                <td>{el.hour_meter.total_hours}</td>
-                                <td>{el.operated_by}</td>
-                                <td>{el.supervised_by}</td>
-                                <td>
-                                    <div className="d-flex">
-                                        <Link to={`/daily-checklist/${el._id}`} 
-                                            className="btn btn-primary btn-sm me-2">Edit</Link>
-                                        <Button variant="danger" size="sm" className="fw-semibold">Remove</Button>
-                                    </div>
-                                </td>
-                            </tr>
-                        )
-                    })}
+                    {/* {data.length <= 0 ? noRecord : displayData} */}
+                    {displayData}
                 </tbody>
             </Table>
         </Container>
