afc1498349e4ad5c0236cb742f91a2bc80affe4f
anmichealgelo <anmichealgelo@gmail.com>
2023-02-03 00:39:30 +0800

    update: fixed layout of form

--- forklift-inspection/src/components/Form.jsx
+++ forklift-inspection/src/components/Form.jsx
@@ -13,222 +13,252 @@ function InspectForm() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(inputs);
+    // alert(inputs);
+    console.log(inputs)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="form-group">
       
       {/* Lift + Operator Info: */}
-      <label><p className='info'>Name:</p>
-      <input className='input'
-        type="text" 
-        name="name" 
-        value={inputs.name || ""} 
-        onChange={handleChange}
-      />
-      </label>
-    
-      <label><p className='info'>Date:</p>
-      <input className='input'
-        type="date" 
-        name="date" 
-        value={inputs.date || ""} 
-        onChange={handleChange}
-      />
-      </label>
+      <div className="col-2">
+        <div className="input-group">
+          <label className='info' htmlFor='name'>Name:</label>
+          <input className='input'
+            type="text" 
+            name="name" 
+            value={inputs.name || ""} 
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="input-group">
+          <label className='info' htmlFor='date'>Date:</label>
+          <input className='input'
+            type="date" 
+            name="date" 
+            value={inputs.date || ""} 
+            onChange={handleChange}
+          />
+        </div>
+      </div>
       
+      <div className="col-2">
+        <dvi className="input-group">
+          <label className='info' htmlFor='lift'>Lift:</label>
+          <input className='input'
+            type="text" 
+            name="lift" 
+            value={inputs.lift || ""} 
+            onChange={handleChange}
+          />
+        </dvi>
 
-      <label><p className='info'>Lift:</p>
-        <input className='input'
-          type="text" 
-          name="lift" 
-          value={inputs.lift|| ""} 
-          onChange={handleChange}
-        />
-        </label>
-
-        <label><p className='info'>Hours:</p>
-        <input className='input'
-          type="numbers" 
-          name="hours" 
-          value={inputs.hours|| ""} 
-          onChange={handleChange}
-        />
-        </label>
-
+        <div className="input-group">
+          <label className='info' htmlFor='hours'>Hours:</label>
+          <input className='input'
+            type="numbers" 
+            name="hours" 
+            value={inputs.hours|| ""} 
+            onChange={handleChange}
+          />
+          
+        </div>
+      </div>
         {/* Maintenance Status: */}
+      <div className="col-3">
+        <div className="input-group">
+          <label className='info' htmlFor='tires'>Tires:</label>
+          <select className='input'
+            type="select" 
+            name="tires" 
+            value={inputs.tires|| PassFail} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
 
-        <label ><p className='test01'>Tires:</p>
-        <select className='input'
-          type="select" 
-          name="tires" 
-          value={inputs.tires|| PassFail} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
+        <div className="input-group">
+          <label className='info' htmlFor='horn'>Horn:</label>
+          <select className='input'
+            type="text" 
+            name="horn" 
+            value={inputs.horn|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+
+        <div className="input-group">
+          <label className='info' htmlFor='battery'>Battery:</label>
+          <select className='input'
+            type="text" 
+            name="battery" 
+            value={inputs.battery|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="col-3">
+        <div className="input-group">
+          <label className='info' htmlFor='controls'>Controls:</label>
+          <select className='input'
+            type="text" 
+            name="controls" 
+            value={inputs.controls|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+
+        <div className="input-group">
+          <label className='info' htmlFor='brakes'>Brakes/Brake Fluid:</label>
+          <select className='input'
+            type="text" 
+            name="brakes" 
+            value={inputs.brakes|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
         
-        <label><p className='test01'>Horn:</p>
-        <select className='input'
-          type="text" 
-          name="horn" 
-          value={inputs.horn|| ""} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
+        <div className="input-group">
+          <label className='info' htmlFor='steering'>Steering:</label>
+          <select className='input'
+            type="text" 
+            name="steering" 
+            value={inputs.steering|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+      </div>
 
-        <label><p className='test01'>Battery:</p>
-        <select className='input'
-          type="text" 
-          name="battery" 
-          value={inputs.battery|| ""} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
+      <div className="col-3">
+        <div className="input-group">
+          <label className='info' htmlFor='hydraulic'>Hydraulics:</label>
+          <select className='input'
+            type="text" 
+            name="hydraulic" 
+            value={inputs.hydraulic|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
 
-        <label><p className='test01'>Controls:</p>
-        <select className='input'
-          type="text" 
-          name="controls" 
-          value={inputs.controls|| ""} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
+        <div className="input-group">
+          <label className='info' htmlFor='hydraulic'>Overhead Guard:</label>
+          <select className='input'
+            type="text" 
+            name="overhead" 
+            value={inputs.overhead|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+        
+        <div className="input-group">
+          <label className='info' htmlFor='charger'>Charger:</label>
+          <select className='input'
+            type="text" 
+            name="charger" 
+            value={inputs.charger|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+      </div>
 
-        <label><p className='test01'>Brakes/Brake Fluid:</p>
-        <select className='input'
-          type="text" 
-          name="brakes" 
-          value={inputs.brakes|| ""} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
+      <div className="col-3">
+        <div className="input-group">
+          <label className='info' htmlFor='fallarrest'>Seat Belt / Fall Arrest:</label>
+          <select className='input'
+            type="text" 
+            name="fallarrest" 
+            value={inputs.fallarrest|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
 
-        <label><p className='test01'>Steering:</p>
-        <select className='input'
-          type="text" 
-          name="steering" 
-          value={inputs.steering|| ""} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
+        <div className="input-group">
+          <label className='info' htmlFor='loadplate'>Is load plate displayed/ free from damage?:</label>
+          <select className='input'
+            type="text" 
+            name="loadplate" 
+            value={inputs.loadplate|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+        
+        <div className="input-group">
+          <label className='info' htmlFor='manual'>Is operators manual present?</label>
+          <select className='input'
+            type="text" 
+            name="manual" 
+            value={inputs.manual|| ""} 
+            onChange={handleChange}
+          >
+            <option value="" disabled></option>
+            <option value="Pass">Pass</option>
+            <option value="Fail">Fail</option>
+          </select>
+        </div>
+      </div>
 
-        <label><p className='test02'>Hydraulics:</p>
-        <select className='input'
-          type="text" 
-          name="hydraulic" 
-          value={inputs.hydraulic|| ""} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
-
-        <label><p className='test02'>Overhead Guard:</p>
-        <select className='input'
-          type="text" 
-          name="overhead" 
-          value={inputs.overhead|| ""} 
-          onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
-
-        <label><p className='test02'>Charger:</p>
-        <select className='input'
-        type="text" 
-        name="charger" 
-        value={inputs.charger|| ""} 
-        onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
-
-        <label><p className='test02'>Seat Belt / Fall Arrest::</p>
-        <select className='input'
-        type="text" 
-        name="fallarrest" 
-        value={inputs.fallarrest|| ""} 
-        onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
-
-        <label><p className='test02'>Is load plate displayed/ free from damage?:</p>
-        <select className='input'
-        type="text" 
-        name="loadplate" 
-        value={inputs.loadplate|| ""} 
-        onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
-
-        <label><p className='test02'>Is operators manual present?:</p>
-        <select className='input'
-        type="text" 
-        name="manual" 
-        value={inputs.manual|| ""} 
-        onChange={handleChange}
-        >
-       <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
-
-        <label><p className='test02'>Is forklift clean, free of trash, excess oil and grease??:</p>
-        <select className='input'
-        type="text" 
-        name="clean" 
-        value={inputs.clean|| ""} 
-        onChange={handleChange}
-        >
-        <option value=""></option>
-        <option value="Pass">Pass</option>
-        <option value="Fail">Fail</option>
-        </select>
-        </label>
-    
-    <input class="button" type="submit" />
+      <div className="col">
+          <div className="input-group">
+            <label className='info' htmlFor='clean'>Is forklift clean, free of trash, excess oil and grease?</label>
+            <select className='input'
+              type="text" 
+              name="clean" 
+              value={inputs.clean|| ""} 
+              onChange={handleChange}
+            >
+              <option value="" disabled></option>
+              <option value="Pass">Pass</option>
+              <option value="Fail">Fail</option>
+            </select>
+          </div>
+      </div>
+  
+    <button className='button' type='submit'>SUBMIT</button>
 </form>
   )
 }
