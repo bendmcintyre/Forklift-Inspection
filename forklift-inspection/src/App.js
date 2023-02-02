f239f64098d332a342f2cb85213357b7985b906b
Ben McIntyre <ben@bens-mbp.attlocal.net>
2023-02-01 17:31:27 -0800

    Updated and switched out useState

--- forklift-inspection/src/App.js
+++ forklift-inspection/src/App.js
@@ -2,39 +2,48 @@ import "./App.css";
 import { Safety } from "./components/Safety.jsx";
 import { Inspect } from "./components/Inspect.jsx";
 import { Contact } from "./components/Contact.jsx";
-
 import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("inspect");
 
   return (
-    <div>
+    <Router>
       <div className="App">
-        {currentPage === "inspect" && <Inspect />}
-        {currentPage === "safety" && <Safety />}
-        {currentPage === "contact" && <Contact />}
+        <Route path="/inspect" component={Inspect} />
+        <Route path="/safety" component={Safety} />
+        <Route path="/contact" component={Contact} />
       </div>
       <div id="navbar-container">
         <nav>
-          <a className={currentPage === "inspect" ? "active" : ""}>
+          <Link
+            to="/inspect"
+            className={currentPage === "inspect" ? "active" : ""}
+          >
             <button id="inspect-page" onClick={() => setCurrentPage("inspect")}>
               Inspect
             </button>
-          </a>
-          <a className={currentPage === "safety" ? "active" : ""}>
+          </Link>
+          <Link
+            to="/safety"
+            className={currentPage === "safety" ? "active" : ""}
+          >
             <button id="safety-page" onClick={() => setCurrentPage("safety")}>
               Safety
             </button>
-          </a>
-          <a className={currentPage === "contact" ? "active" : ""}>
+          </Link>
+          <Link
+            to="/contact"
+            className={currentPage === "contact" ? "active" : ""}
+          >
             <button id="contact-page" onClick={() => setCurrentPage("contact")}>
               Contact
             </button>
-          </a>
+          </Link>
         </nav>
       </div>
-    </div>
+    </Router>
   );
 }
 
