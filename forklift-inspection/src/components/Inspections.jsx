9bb289853714f4e97ce0f29a409068205529b155
anmichealgelo <anmichealgelo@gmail.com>
2023-02-04 00:24:07 +0800

    update: added edit view

--- forklift-inspection/src/components/Inspections.jsx
+++ forklift-inspection/src/components/Inspections.jsx
@@ -11,13 +11,10 @@ export default function Inspections() {
   const fetchData = async () => {
     document.title = "Inspections"
 
-    const rawResposne = await fetch(API_URL + "/inspections/", {
-      headers: {
-        mode: 'cors'
-      }
-    })
-
+    const rawResposne = await fetch(API_URL + "/inspections/")
     const response = await rawResposne.json()
+
+    // console.log(response)
 
     if(response.count !== 0){
       setData(response.data)
@@ -48,7 +45,7 @@ export default function Inspections() {
         <td>{el.lift}</td>
         <td>{el.hours}</td>
         <td>
-          <Link to={`/inspections/${el._id}`} className="button-like">Edit</Link>
+          <Link to={`/inspect-form?id=${el._id}`} className="button-like">Edit</Link>
           <button onClick={() => {
             if(window.confirm("Are you sure you want to remove?")){
               removeData(el._id)
