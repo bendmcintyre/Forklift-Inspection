d872219d0f7cfe8207e206a28f9337df2e0be425
Ben McIntyre <ben@Bens-MBP.lan>
2023-02-04 16:23:27 -0800

    Updates to navbar styling

--- forklift-inspection/src/components/Header.jsx
+++ forklift-inspection/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header(){
             {/* For routing we need match the route name in Route path attribute */}
             <Link to="/home">HOME</Link>
             <Link to="/inspections">INSPECTIONS</Link>
-            <Link to="/contact">CONTACT</Link>
             <Link to="/safety">SAFETY</Link>
+            <Link to="/contact">CONTACT</Link>
           </nav>
         </div>
     )
