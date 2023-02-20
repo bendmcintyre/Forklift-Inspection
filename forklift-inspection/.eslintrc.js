845868c421219fb6ace3803249189087a5f9f9ab
Myke Ahn-Stubbs <myke@mykestubbs.com>
2023-02-19 23:10:29 -0800

    Add eslint config

---
+++ forklift-inspection/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'max-len': ['warn'],
+    'quote-props': ['error', 'consistent'],
+    'object-curly-newline': ['error', {
+      ImportDeclaration: 'always',
+    }],
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    // TODO: Enable this once id columns are safely renamed
+    'no-underscore-dangle': 'off',
+  },
+};
