f0912157289a517939d1e61125a00398b1754a06
Myke Ahn-Stubbs <myke@mykestubbs.com>
2023-02-26 02:05:38 -0800

    Add InspectionsProvider to App

--- forklift-inspection/src/App.js
+++ forklift-inspection/src/App.js
@@ -1,32 +1,40 @@
-import './App.css';
-
+import {
+  React,
+} from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom'; // this is for routing purposes
+
+import './App.css';
+
 import TopBar from './components/TopBar';
 import Reminders from './components/Reminders';
-
 import Contact from './components/Contact';
 import InspectForm from './components/Form';
 import Inspections from './components/Inspections';
+import {
+  InspectionsProvider,
+} from './contexts/InspectionsContext';
 
 function App() {
   return (
-    <Router>
-      <TopBar />
+    <InspectionsProvider>
+      <Router>
+        <TopBar />
 
-      {/* Component that'll match the routes name will be rendered here */}
-      <div className="container mx-auto max-w-7xl">
-        <Routes>
-          <Route path="/inspections" element={<Inspections />} />
-          <Route path="/reminders" element={<Reminders />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/inspect-form" element={<InspectForm />} />
-        </Routes>
-      </div>
-    </Router>
+        {/* Component that'll match the routes name will be rendered here */}
+        <div className="container mx-auto max-w-7xl">
+          <Routes>
+            <Route path="/inspections" element={<Inspections />} />
+            <Route path="/reminders" element={<Reminders />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/inspect-form" element={<InspectForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </InspectionsProvider>
   );
 }
 
